1b049d874edbf154756e84aa50e1e03f73a6540b
virusphp <virusp_php@yahoo.com>
2024-09-03 08:21:33 +0700

    set config satusehat consenet and kfa

--- src/Config/ConfigSatusehat.php
+++ src/Config/ConfigSatusehat.php
@@ -6,60 +6,73 @@ use Dotenv\Dotenv;
 
 class ConfigSatusehat
 {
-	protected $urlAuth;
-	protected $urlBase;
-	protected $urlConsent;
+    protected $urlAuth;
+    protected $urlBase;
+    protected $urlConsent;
+    protected $urlKfa;
+    protected $urlKfaV2;
     protected $clientId;
     protected $clientSecret;
     protected $token;
     protected $header;
     protected $timestamps;
 
-	public function __construct()
+    public function __construct()
     {
         $dotenv = Dotenv::createUnsafeImmutable(getcwd());
-		$dotenv->safeLoad();
+        $dotenv->safeLoad();
 
         $this->urlAuth = getenv('API_SATUSEHAT_AUTH');
         $this->urlBase = getenv('API_SATUSEHAT_BASE');
         $this->urlConsent = getenv('API_SATUSEHAT_CONSENT');
+        $this->urlKfa = getenv('API_SATUSEHAT_KFA');
+        $this->urlKfaV2 = getenv('API_SATUSEHAT_KFA_V2');
         $this->clientId = getenv('CLIENT_ID_SATUSEHAT');
         $this->clientSecret = getenv('CLIENT_SECRET_SATUSEHAT');
     }
 
-	public function setUrlAuth()
+    public function setUrlAuth()
     {
-       return $this->urlAuth; 
+        return $this->urlAuth;
     }
 
-	public function setUrlBase()
+    public function setUrlBase()
     {
-       return $this->urlBase; 
+        return $this->urlBase;
     }
 
-	public function setUrlConsent()
+    public function setUrlConsent()
     {
-       return $this->urlConsent;  
+        return $this->urlConsent;
+    }
+
+    public function setUrlKfa()
+    {
+        return $this->urlKfa;
+    }
+
+    public function setUrlKfaV2()
+    {
+        return $this->urlKfaV2;
     }
 
     public function setClientId()
     {
-       return $this->clientId; 
+        return $this->clientId;
     }
 
     public function setClientSecret()
     {
-       return $this->clientSecret; 
+        return $this->clientSecret;
     }
 
-	public function setCredentials()
-	{
-		$data = [
-			"client_id" => $this->clientId,
-  			"client_secret" => $this->clientSecret
-		];
+    public function setCredentials()
+    {
+        $data = [
+            "client_id" => $this->clientId,
+            "client_secret" => $this->clientSecret
+        ];
 
-		return http_build_query($data);
-	}
-
+        return http_build_query($data);
+    }
 }
--- src/Config/satusehat.php
+++ src/Config/satusehat.php
@@ -2,9 +2,11 @@
 
 return [
 	'api' => [
-		'endpoint_auth'  => env('API_SATUSEHAT_AUTH','ENDPOINT-KAMU'),
-		'endpoint_base'  => env('API_SATUSEHAT_BASE','ENDPOINT-KAMU'),
-		'endpoint_consent'  => env('API_SATUSEHAT_CONSENT','ENDPOINT-KAMU'),
+		'endpoint_auth'  => env('API_SATUSEHAT_AUTH', 'ENDPOINT-KAMU'),
+		'endpoint_base'  => env('API_SATUSEHAT_BASE', 'ENDPOINT-KAMU'),
+		'endpoint_consent'  => env('API_SATUSEHAT_CONSENT', 'ENDPOINT-KAMU'),
+		'endpoint_kfa'  => env('API_SATUSEHAT_KFA', 'ENDPOINT-KAMU'),
+		'endpoint_kfa_v2'  => env('API_SATUSEHAT_KFA_V2', 'ENDPOINT-KAMU'),
 		'client_id' => env('CLIENT_ID_SATUSEHAT', 'SECRET-KAMU'),
 		'client_secret' => env('CLIENT_SECRET_SATUSEHAT', 'SECRET-KAMU'),
 	]
